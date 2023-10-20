c41953ad1c41a71316d7cc0e824cf8d9c201edc2
Javier Jamaica <javiernicolas.jamaica@ikasle.egibide.org>
2023-10-20 13:00:37 +0200

    Primer commit

---
+++ 01-Introduccion/Ejercicio1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $nombre = "Mikel";
+    $edad = 22;
+    ?>
+    <?= "Hola soy $nombre y tengo $edad años" ?>
+</body>
+
+</html>
