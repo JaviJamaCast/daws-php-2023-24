bec2e6876d3dc0a911f576725d76919fbe1038cb
Javier Jamaica <javiernicolas.jamaica@ikasle.egibide.org>
2023-10-20 13:03:22 +0200

    Ejercicio2

---
+++ 01-Introduccion/Ejercicio2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $nombre = "Miguel";
+    $edad = 22;
+    ?>
+    <?= "Mi amigo $nombre y tengo $edad años" ?>
+</body>
+
+</html>
